22551291ab4edae33d3f41bcaf78f81ed894f84e
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2018-11-12 23:04:50 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,24 +15,6 @@ client.on('message', message => {
 	}
 });
 
-client.on('message', message => {
-	if (message.author === client.user) return;
-	if (message.content.startsWith(prefix + 'ping2')) {
-	const embed = new Discord.RichEmbed()
-  .setAuthor("Yu", "https://cdn.discordapp.com/avatars/511749990167740440/9de74058348dc21c675c49721702b3b0.jpg?size=1024")
-  /*
-   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-   */
-  .setColor(0xFFBAFA)
-  .setDescription("xd")
-  /*
-   * Takes a Date object, defaults to current date.
-   */
-  .setTimestamp()
-  
 
-  message.channel.send({embed});
-	}
-});
 client.login(process.env.BOT_TOKEN);
 
