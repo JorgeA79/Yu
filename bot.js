79cd51c52a2e2e3a99a55a4c3710853e20e6eb23
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-14 01:27:49 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 
 client.on('guildMemberAdd', member => {
-    member.guild.channels.get('694781619306889266').send('Welcome TO THE SERVER! **' + member.user.username + '**,  Go introduce yourself in #introductions! If you want, you can go to #club-info and to join a club, go to #club-applications'); 
+    member.guild.channels.get('694781619306889266').send('Welcome TO THE SERVER! **' + member.user + '**,  Go introduce yourself in <#696322478922006588>! If you want, you can go to <#709716878263845218> and to join a club, go to <#709718943660769310>'); 
 });
   
 
