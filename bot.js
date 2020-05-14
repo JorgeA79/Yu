41590abd320d4fe9aac10540d782a63a7c0f85c4
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-14 01:32:19 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('message', message => {
 });
 
 client.on('guildMemberAdd', member => {
-    member.guild.channels.get('694781619306889266').send('Welcome TO THE SERVER! **' + member.user + '**,  Go introduce yourself in <#696322478922006588>! If you want, you can go to <#709716878263845218> and to join a club, go to <#709718943660769310>'); 
+    member.guild.channels.get('694781619306889266').send('Welcome TO THE SERVER! **' + member.user.username + '**,  Go introduce yourself in <#696322478922006588>! If you want, you can go to <#709716878263845218> and to join a club, go to <#709718943660769310>'); 
 });
   
 
