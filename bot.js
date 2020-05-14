75b27350bdf1fda95749d35cd93645086e7e7203
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-14 01:29:40 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var prefix = "p!"
 
+ 
+client.on('message', message => {
+	if (message.author === client.user) return;
+	if (message.content.startsWith(prefix + 'ping')) {
+	
+		 message.channel.send(`Pong! :ping_pong:  \`${Date.now() - message.createdTimestamp} ms\``);
+	}
+});
 
 client.on('guildMemberAdd', member => {
     member.guild.channels.get('694781619306889266').send('Welcome TO THE SERVER! **' + member.user + '**,  Go introduce yourself in <#696322478922006588>! If you want, you can go to <#709716878263845218> and to join a club, go to <#709718943660769310>'); 
