c7cc8d9e7a8c182ee2811166775e5b686c4644e8
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-15 01:17:03 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,26 @@ const client = new Discord.Client();
  //youtubeKey: 'AIzaSyDCk1-hiwXO7PhT27ZuBRXIfhrrIHuhAOc'
 
 
+const MusicClient = require("discord-music-core")
+    const musicPlayer = new MusicClient("AIzaSyDCk1-hiwXO7PhT27ZuBRXIfhrrIHuhAOc")
+    musicPlayer.play(message, searchArray) // Searches for and add the searched results to the queue.
+    musicPlayer.playTop(message, searchArray) // Searches for and add the searched results to the top of the queue.
+    musicPlayer.stop(message) // Stop the music, which includes clearing the queue.
+    musicPlayer.nowPlaying(message) // Display the music title for now playing.
+    musicPlayer.showQueue(message) // Displays the whole music queue.
+    musicPlayer.skip(message) // Skip the song that is now playing.
+    musicPlayer.remove(message) // Removes certain position of song in the song queue.
+    musicPlayer.pause(message) // Pauses the music queue.
+    musicPlayer.repeat(message) // Repeats the current song.
+    musicPlayer.loop(message) // Loops the whole queue.
+    musicPlayer.shuffle(message) // Shuffles the whole queue.
+    musicPlayer.volume(message, volume) // Sets the volume to the certain amount. Using negative values will show the volume that is using now instead.
+
+
+
+
+
+
 client.on('ready',() => {
 	
 	   client.user.setPresence({game: {name: "in JAPAN | p!help", type: 0}});
