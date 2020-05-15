d441c27a3f88e38c3fe526c6f1bc1d5e1c013b00
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-15 13:57:30 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -327,9 +327,11 @@ client.on('message', message => {
 		
       return message.channel.sendMessage(":x: You are not in a voice channel!!");
     }
-	message.channel.sendMessage(":white_check_mark: **Connected!**");
+	
 	    message.member.voiceChannel.join()
-    
+     		.then(connection => {
+		    message.channel.sendMessage(":white_check_mark: **Connected!**");
+	    })
 	}
 	});
 
