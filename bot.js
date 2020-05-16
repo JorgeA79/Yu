08c312d497e3a31fb293184966e09dbc82db1a47
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-15 20:06:39 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -304,6 +304,7 @@ client.on('message', message => {
 	if (message.author === client.user) return;
 	if (message.content.startsWith(prefix + 'say')) {
 		const args = message.content.slice(prefix.length).split(' ');
+		 args = args.split(',').map(elem => elem.trim());
 		if (!args.length) {
 		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
 	}
