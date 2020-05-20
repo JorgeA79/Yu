e54122fded69050ab2dadb0461352730df1f78a3
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-20 04:55:22 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -826,10 +826,8 @@ client.on('message', message => {
 		
 		search(argsowo, opts, function(err, results) {
   		if(err) return console.log(err);
-  		console.dir(results);	
-		var resultowo = results.results;
-		var selectedR = resultowo[1];
-		message.channel.sendMessage(selectedR.link);
+  		console.dir(links);	
+		
 	});
 	}
 	});
