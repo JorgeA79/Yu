e11d885463d6636df542fa7b6327c36157e2943b
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-20 00:21:16 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -766,7 +766,7 @@ client.on('message', message => {
 	return message.channel.sendMessage(":x: You are not in a voice channel!!");
     	}
 	message.channel.sendMessage(":white_check_mark: **Connected!**");
-    	
+    	message.channel.sendMessage("${args}");
 		
 		let stream = ytdl("https://www.youtube.com/watch?v=VcyFfcJbyeM", {
             filter: "audioonly",
