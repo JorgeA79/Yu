6d4cb5676bf46b9333fef1de18d9f68099432f4a
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-05-22 01:46:34 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -177,7 +177,7 @@ var patgifs =  [
 ///////////////////////////////////////// C O M M A N D S /////////////////////////////////////////
 
 client.on('guildMemberAdd', member => {
-    member.guild.channels.get('694781619306889266').send('WELCOME TO THE SERVER!! **' + member.user.username + '**,  Go introduce yourself in <#696322478922006588>! If you want, you can go to <#709716878263845218> and to join a club, go to <#709718943660769310>, You can also get your own roles in <#710829476971544596>!"'); 
+    member.guild.channels.get('694781619306889266').send('WELCOME TO THE SERVER!! **' + member.user.username + '**,  Go introduce yourself in <#696322478922006588>! If you want, you can go to <#709716878263845218> and to join a club, go to <#709718943660769310>, You can also get your own roles in <#710829476971544596>!'); 
 });
  
 ////////////////////////////////////////////////////////////////////////////////////////////////////
